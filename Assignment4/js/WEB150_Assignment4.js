9ab658d3e3951f8e9ea9649b30d8c08ad8ae7920
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-03 10:51:25 -0800

    Assignment 4 files

---
+++ Assignment4/js/WEB150_Assignment4.js
@@ -0,0 +1,15 @@
+function calculateExponent(number1, exponent1)
+{
+    //OBJECT
+    this.number1 = number1;
+    this.exponent1 = exponent1;
+    //METHOD
+    return (Math.pow(this.number1, this.exponent1));
+}
+
+var Calc1 = new calculateExponent(num1, num2);
+var elCalc1 = document.getElementById('calculate');
+
+Calc1 += " ";
+elCalc1.textContent = "The sum of " + num1 + " and " + num2 + " is: " + Calc1;
+console.log(Calc1);
