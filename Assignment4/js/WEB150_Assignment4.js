ba0361c0daf39f5df274d008a9956623df4caa3d
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-03 11:40:22 -0800

    update syntax

--- Assignment4/js/WEB150_Assignment4.js
+++ Assignment4/js/WEB150_Assignment4.js
@@ -11,5 +11,5 @@ var Calc1 = new calculateExponent(num1, num2);
 var elCalc1 = document.getElementById('calculate');
 
 Calc1 += " ";
-elCalc1.textContent = "The power of " + num1 + " to " + num2 + " is: " + Calc1;
+elCalc1.textContent = "The power of " + Calc1.number1 + " to " + Calc1.exponent1 + " is: " + Calc1;
 console.log(Calc1);
