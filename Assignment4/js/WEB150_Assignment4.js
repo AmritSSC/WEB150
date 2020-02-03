7b95add79a1b6af9ca0e9add173fcc2b503e1cf1
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-03 11:07:33 -0800

    edited text

--- Assignment4/js/WEB150_Assignment4.js
+++ Assignment4/js/WEB150_Assignment4.js
@@ -11,5 +11,5 @@ var Calc1 = new calculateExponent(num1, num2);
 var elCalc1 = document.getElementById('calculate');
 
 Calc1 += " ";
-elCalc1.textContent = "The sum of " + num1 + " and " + num2 + " is: " + Calc1;
+elCalc1.textContent = "The power of " + num1 + " to " + num2 + " is: " + Calc1;
 console.log(Calc1);
