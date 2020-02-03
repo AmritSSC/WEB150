f7fbb6265db424fb1ce6c3d7cf06f5d13bb8960b
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-03 11:57:28 -0800

    Updated syntax

--- Assignment4/js/WEB150_Assignment4.js
+++ Assignment4/js/WEB150_Assignment4.js
@@ -1,14 +1,19 @@
+//object constructor
 function calculateExponent(number1, exponent1)
 {
     //OBJECT
     this.number1 = number1;
     this.exponent1 = exponent1;
     //METHOD
-    return (Math.pow(this.number1, this.exponent1));
+    this.checkResult = function(){
+        return Math.pow(this.number1, this.exponent1);
+    }
 }
 
+//define instance
 var Calc1 = new calculateExponent(num1, num2);
 var elCalc1 = document.getElementById('calculate');
 
-elCalc1.textContent = "The power of " + Calc1.number1 + " to " + Calc1.exponent1 + " is: " + Calc1;
-console.log(Calc1);
+get result:
+//elCalc1.textContent = "The power of " + Calc1.number1 + " to " + Calc1.exponent1 + " is: " + Math.pow(Calc1.number1, Calc1.exponent1);
+elCalc1.textContent = "The power of " + Calc1.number1 + " to " + Calc1.exponent1 + " is: " + Calc1.checkResult();
