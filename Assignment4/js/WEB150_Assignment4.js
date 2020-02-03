a05a6b96c9a209c08e3a0502ccb914bc2787b5e7
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-03 11:59:52 -0800

    Update syntax

--- Assignment4/js/WEB150_Assignment4.js
+++ Assignment4/js/WEB150_Assignment4.js
@@ -9,11 +9,8 @@ function calculateExponent(number1, exponent1)
         return Math.pow(this.number1, this.exponent1);
     }
 }
-
-//define instance
+    
 var Calc1 = new calculateExponent(num1, num2);
 var elCalc1 = document.getElementById('calculate');
 
-get result:
-//elCalc1.textContent = "The power of " + Calc1.number1 + " to " + Calc1.exponent1 + " is: " + Math.pow(Calc1.number1, Calc1.exponent1);
 elCalc1.textContent = "The power of " + Calc1.number1 + " to " + Calc1.exponent1 + " is: " + Calc1.checkResult();
