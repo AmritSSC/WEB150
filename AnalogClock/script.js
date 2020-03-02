d37ad3af720ff3f04cbf94ed48a2f62a6ca5da36
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-03-02 12:44:16 -0800

    update incomplete command

--- AnalogClock/script.js
+++ AnalogClock/script.js
@@ -28,7 +28,7 @@ function runTheClock()
 
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.transform = "rotate(" + secPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
 var interval = setInterval(runTheClock, 1000);
